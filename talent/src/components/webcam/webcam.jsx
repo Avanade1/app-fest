b18cbb9c65dd463be8a3ff263a38634e55556696
Karina <karina.souza1981@gmail.com>
2020-09-10 18:24:20 -0300

    webcam

--- talent/src/components/webcam/webcam.jsx
+++ talent/src/components/webcam/webcam.jsx
@@ -6,14 +6,11 @@ import "./webcam.css";
 
 const WebcamCapture = (props) => {
   const webcamRef = React.useRef(null);
-  // const [imgSrc, setImgSrc] = React.useState(null);
 
   const { callBack } = props;
 
   const capture = React.useCallback(() => {
     var imageSrc = webcamRef.current.getScreenshot();
-    // console.log(imageSrc);
-    // setImgSrc(imageSrc);
     var buff = new Buffer(
       imageSrc.replace(/^data:image\/(png|gif|jpeg);base64,/, ""),
       "base64"
@@ -21,33 +18,6 @@ const WebcamCapture = (props) => {
 
     callBack(buff);
   }, [webcamRef, callBack]);
-
-  //   var options = {
-  //     method: 'POST',
-  //     url: 'https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect',
-  //     qs: { returnFaceId: 'true', recognitionModel: 'recognition_02' },
-  //     headers:
-  //     {
-  //         // 'Postman-Token': 'de6cd04f-30da-483d-ad6a-fad5a4aaf4bb',
-  //         // 'cache-control': 'no-cache',
-  //         'Content-Type': 'application/octet-stream',
-  //         'Ocp-Apim-Subscription-Key': '8048147a92dc4690bea5e4cc82532804'
-  //     },
-  //     body: buff
-  // };
-  // https://facelaboratoria.cognitiveservices.azure.com/face/v1.0/detect[?returnFaceId]
-  // [&returnFaceLandmarks]
-  // [&returnFaceAttributes]
-  // [&recognitionModel]
-  // [&returnRecognitionModel]
-  // [&detectionModel]
-
-  // const registerImg = () => {
-  //   // fetch ("https://facelaboratoria.cognitiveservices.azure.com/")
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then(resp => console.log(resp.json()))
-  //     .catch(err => console.log(err))
-  // }
 
   const videoConstraints = {
     width: 800,
