919175cfb482da2b8a2fe053b9edbb3082e20857
Karina <karina.souza1981@gmail.com>
2020-09-10 11:58:40 -0300

    callback in component webcam and recognit person face

--- talent/src/components/webcam/webcam.jsx
+++ talent/src/components/webcam/webcam.jsx
@@ -1,76 +1,26 @@
-import React from 'react';
-import Webcam from 'react-webcam';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { Theme, Button } from 'laboratoria-ui';
-import './webcam.css';
+import React from "react";
+import Webcam from "react-webcam";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { Theme, Button } from "laboratoria-ui";
+import "./webcam.css";
 
-// Import da lib azure
-import { FaceClient } from '@azure/cognitiveservices-face';
-import { CognitiveServicesCredentials } from '@azure/ms-rest-azure-js';
-
-const personGroupId = "avanade1";
-
-// configura a conexao com a api da azure
-const client = new FaceClient(
-  new CognitiveServicesCredentials("64bc1a006e25491a9d0dfee37ac6ebd9"),
-  "https://facelaboratoria.cognitiveservices.azure.com/"
-);
-
-
-const WebcamCapture = () => {
+const WebcamCapture = (props) => {
   const webcamRef = React.useRef(null);
-  const [imgSrc, setImgSrc] = React.useState(null);
+  // const [imgSrc, setImgSrc] = React.useState(null);
+
+  const { callBack } = props;
 
   const capture = React.useCallback(() => {
     var imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
-    setImgSrc(imageSrc);
+    // console.log(imageSrc);
+    // setImgSrc(imageSrc);
     var buff = new Buffer(
       imageSrc.replace(/^data:image\/(png|gif|jpeg);base64,/, ""),
       "base64"
     );
 
-    //  para treinar o grupo de imagens no azure
-    // client.personGroup.train(personGroupId)
-
-    // verifica se terminou o treino do grupo de imagens
-    client.personGroup.getTrainingStatus(personGroupId)
-        .then(trainStatus => {
-            if (trainStatus.status === 'succeeded') {
-              // chama o endpoint da azure para tentar detectar a face na imagem que foi capturada pela camera foto da foto
-              client.face.detectWithStream(buff, {returnFaceId:true}).then(async face => {
-                try {
-                  if (face.length === 0) {
-                      alert('Não foi possivel encontrar uma face na imagem, tente novamente!')
-                      return false;
-                  }
-
-                  const faceId = face[0].faceId
-                  console.log("faceId...", faceId);
-                  try {
-                    const clientIdentify = await client.face.identify([faceId], {personGroupId: personGroupId})
-                    console.log("clientIdentify...", clientIdentify);
-
-
-                  } catch(erro) {
-                    console.log('erro ao tentar identificar', erro)
-                    alert('O usuario nao esta cadastrado na base de dados')
-                  }
-                } catch(err) {
-                    //console.log(err)
-                    alert('Erro ao capturar imagem, tente novamente!')
-                }
-              })
-            } else {
-                alert('O grupo esta sendo treinado, aguarde para fazer a validação')
-            }
-        }).catch(err => {
-          alert(`erro ${err}`);
-          console.log("train....", err)
-        })            
-
-
-  }, [webcamRef, setImgSrc]);
+    callBack(buff);
+  }, [webcamRef, callBack]);
 
   //   var options = {
   //     method: 'POST',
@@ -102,18 +52,18 @@ const WebcamCapture = () => {
   const videoConstraints = {
     width: 800,
     height: 800,
-    facingMode: "user"
+    facingMode: "user",
   };
 
   return (
     <MuiThemeProvider theme={Theme}>
-      <div className='center-block'>
+      <div className="center-block">
         <Webcam
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
           mirrored={true}
-          className='photo'
+          className="photo"
           width={800}
           height={800}
           videoConstraints={videoConstraints}
--- talent/src/pages/App.js
+++ talent/src/pages/App.js
@@ -1,20 +1,105 @@
-import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { Theme } from 'laboratoria-ui';
-import Typography from '@material-ui/core/Typography';
-import './App.css';
-import logo from './../img/logo.svg'; 
-import WebcamCapture from './../components/webcam/webcam' 
-import Footer from './../components/footer/footer'
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { Theme } from "laboratoria-ui";
+import Typography from "@material-ui/core/Typography";
+import "./App.css";
+import logo from "./../img/logo.svg";
+import WebcamCapture from "./../components/webcam/webcam";
+import Footer from "./../components/footer/footer";
 
-function App() {
+// Import da lib azure
+import { FaceClient } from "@azure/cognitiveservices-face";
+import { CognitiveServicesCredentials } from "@azure/ms-rest-azure-js";
+
+const personGroupId = "facelaboratoria";
+
+// configura / cria a conexao com a api da azure
+const client = new FaceClient(
+  new CognitiveServicesCredentials("64bc1a006e25491a9d0dfee37ac6ebd9"),
+  "https://facelaboratoria.cognitiveservices.azure.com/"
+);
+
+const App = () => {
+  const callBackCaptureImage = function (file) {
+    //  para treinar o grupo de imagens no azure
+    // client.personGroup.train(personGroupId);
+
+    // verifica se terminou o treino do grupo de imagens
+    client.personGroup
+      .getTrainingStatus(personGroupId)
+      .then((trainStatus) => {
+        if (trainStatus.status === "succeeded") {
+          // chama o endpoint da azure para tentar detectar a face na imagem que foi capturada pela camera
+          // o azure cria um faceId temporario
+          client.face
+            .detectWithStream(file, { returnFaceId: true })
+            .then(async (face) => {
+              if (face.length === 0) {
+                alert(
+                  "Não foi possivel encontrar uma face na imagem, tente novamente!"
+                );
+                return false;
+              }
+
+              const faceId = face[0].faceId;
+              try {
+                // chama o endpoint da azure para identificar se o faceId
+                const clientIdentify = await client.face.identify([faceId], {
+                  personGroupId: personGroupId,
+                });
+
+                if (
+                  clientIdentify.length === 0 ||
+                  clientIdentify[0].candidates.length === 0
+                ) {
+                  alert("O usuario não está cadastrado na base de dados");
+                  return false;
+                }
+
+                // pega o primeiro canditado encontrado com face parecida
+                const personId = clientIdentify[0].candidates[0].personId;
+
+                // verifica se o faceId gerado temporariamente é da personId do primeiro candidato
+                const resultVerifyPerson = await client.face.verifyFaceToPerson(
+                  faceId,
+                  personId,
+                  { personGroupId: personGroupId }
+                );
+
+                if (resultVerifyPerson.isIdentical === true) {
+                  // Se o resultado for identico, ou seja se encontrou uma face e uma pessoa cadastrada no grupo
+                  // Busca no azure a informação da pessoa que foi encontrada
+                  const person = await client.personGroupPerson.get(
+                    personGroupId,
+                    personId
+                  );
+
+                  alert(`Ola ${person.name} seja muito bem vinda.... `);
+                } else {
+                  alert("Não encontrou face equivalente");
+                }
+              } catch (err) {
+                console.log("erro ao tentar identificar", err);
+                alert("O usuario nao esta cadastrado na base de dados");
+              }
+            });
+        } else {
+          alert("O grupo esta sendo treinado, aguarde para fazer a validação");
+        }
+      })
+      .catch((err) => {
+        alert(`erro ${err}`);
+        console.log("train....", err);
+      });
+  };
+
   return (
     <MuiThemeProvider theme={Theme}>
       <header>
-        <img alt='Logo Laboratoria' src={logo}></img>
+        <img alt="Logo Laboratoria" src={logo}></img>
       </header>
-      <section>        
-        <div className='space-big'>
+      <section>
+        <div className="space-big">
           <Typography variant="h1" align="center">
             Talent Fest
           </Typography>
@@ -22,7 +107,7 @@ function App() {
             2021
           </Typography>
         </div>
-        <div className='space-small'>
+        <div className="space-small">
           <Typography variant="h3" align="center">
             Olá! Bem-vinde!
           </Typography>
@@ -30,12 +115,10 @@ function App() {
             Estamos te aguardando :D
           </Typography>
         </div>
-        <WebcamCapture  />
-      </section> 
-    
+        <WebcamCapture callBack={callBackCaptureImage} />
+      </section>
     </MuiThemeProvider>
-    
   );
-}
+};
 
 export default App;
