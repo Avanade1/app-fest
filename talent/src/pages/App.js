c32ef75e3d9e40ffed826e0a61471b6e92ad72fe
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-09-10 18:05:15 -0300

    Arquivo de rotas

--- talent/src/pages/App.js
+++ talent/src/pages/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { Theme } from 'laboratoria-ui';
-import Typography from '@material-ui/core/Typography';
 import './App.css';
-import logo from './../img/logo.svg'; 
-import WebcamCapture from './../components/webcam/webcam' 
-import Footer from './../components/footer/footer'
+import Register from './cadastro/cadastros'
+import Login from './login/login'
+
+
 
 function App() {
   return (
     <MuiThemeProvider theme={Theme}>
-      <header>
-        <img alt='Logo Laboratoria' src={logo}></img>
-      </header>
-      <section>        
-        <div className='space-big'>
-          <Typography variant="h1" align="center">
-            Talent Fest
-          </Typography>
-          <Typography variant="h2" align="center">
-            2021
-          </Typography>
-        </div>
-        <div className='space-small'>
-          <Typography variant="h3" align="center">
-            Olá! Bem-vinde!
-          </Typography>
-          <Typography variant="subtitle1" align="center">
-            Estamos te aguardando :D
-          </Typography>
-        </div>
-        <WebcamCapture  />
-      </section> 
-    
+      {/*<Register/>*/}
+    <Login/>
     </MuiThemeProvider>
     
   );
